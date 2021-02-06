0e2e62112d4c658fecc909184fc8bc6043c2bdc8
jibraelp <73617082+jibraelp@users.noreply.github.com>
2021-02-06 16:03:36 -0500

    Add files via upload
    A musical if statement

---
+++ ifExercise_2.py
@@ -0,0 +1,16 @@
+
+name = input('Enter your name: ')
+age = int(input('Enter your age: '))
+
+
+
+if age > 60:
+    print (name + ' ,you like Michael Jackson')
+elif age > 40:
+    print(name + ' ,you like the the Beatles')
+elif age > 30:
+    print (name + ' ,you like Tupac')
+elif age > 15:
+    print(name + ' ,you like Drake')
+else:
+    print(name + ',you do not like anything!')
